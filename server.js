8b803ff52fcd67730c84069758bffb42c18d02f9
Karlo Dujmic <kd48376@oss.unist.hr>
2022-12-20 15:44:12 +0100

    Add triggers for cron fuctions on the server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
+const cron = require('node-cron');
+
+const expired_appointment = require('./service/expired_appointment');
+
 dotenv.config({ path: './config.env' });
 
 // Connection to the DB, using the variables from the config.env file
@@ -18,6 +22,10 @@ mongoose
 	.then(() => console.log('DB connection established'));
 
 const port = 3000 || process.env.PORT;
+
+cron.schedule('0 * * * * *', function () {
+	expired_appointment.expired_appointment();
+});
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}...`);
