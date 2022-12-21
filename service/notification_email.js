e6978a9dce8efa4730199d4d450a13a0ff0d81b8
Karlo Dujmic <kd48376@oss.unist.hr>
2022-12-21 16:24:31 +0100

    Remove send email function from notification_email file

--- service/notification_email.js
+++ service/notification_email.js
@@ -1,6 +1,6 @@
 const User = require('../models/user_model');
 const Appointment = require('../models/appointment_model');
-const nodemailer = require('nodemailer');
+const mail = require('./send_email')
 
 exports.reminder_email = async () => {
 	const appointments = await Appointment.find();
@@ -22,40 +22,15 @@ exports.reminder_email = async () => {
 			patient[0].name
 		}, you have scheduled an appointment with Dr.${
 			doctor[0].name
-		} at ${this.visit_date.toLocaleString('en-US', {
+		} at ${appointment.visit_date.toLocaleString('en-US', {
 			dateStyle: 'medium',
 			timeZone: 'UTC',
 			timeStyle: 'short',
 			hour12: true,
 		})}.`;
 
-		this.send_mail(patient, doctor, subject, text);
+		mail.send_mail(patient, doctor, subject, text);
 	});
 };
 
-exports.send_mail = (patient, doctor, subject, text) => {
-	const transporter = nodemailer.createTransport({
-		service: 'gmail',
-		auth: {
-			user: process.env.EMAIL,
-			pass: process.env.EMAIL_PASSWORD,
-		},
-	});
 
-	// Fill out mail options with the infromation needed
-	const mailOptions = {
-		from: 'kdujmic10@gmail.com',
-		to: `${patient[0].email}`,
-		subject: subject,
-		text: text,
-	};
-
-	// Send email or catch the error e.g. no valid email to send
-	transporter.sendMail(mailOptions, function (error, info) {
-		if (error) {
-			console.log(error);
-		} else {
-			console.log('Email sent: ' + info.response);
-		}
-	});
-};
