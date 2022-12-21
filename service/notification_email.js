7c203cede3f5067a36a72cc07333348b7f7a2293
Karlo Dujmic <kd48376@oss.unist.hr>
2022-12-21 14:48:06 +0100

    Add send_email function

--- service/notification_email.js
+++ service/notification_email.js
@@ -2,9 +2,10 @@ const User = require('../models/user_model');
 const Appointment = require('../models/appointment_model');
 const nodemailer = require('nodemailer');
 
-exports.notification_email = async () => {
+exports.reminder_email = async () => {
 	const appointments = await Appointment.find();
 
+	// Filter out the appointments that are scheduled for today
 	const active_appointments = appointments.filter(
 		(el) =>
 			el.visit_date.toDateString() === new Date().toDateString() &&
@@ -12,42 +13,49 @@ exports.notification_email = async () => {
 			el.doctor !== null
 	);
 
+	// Send email to every patient who has an appointment today
 	active_appointments.forEach(async (appointment) => {
 		const doctor = await User.find(appointment.doctor);
 		const patient = await User.find(appointment.patient);
+		const subject = 'Doctor appointment';
+		const text = `${
+			patient[0].name
+		}, you have scheduled an appointment with Dr.${
+			doctor[0].name
+		} at ${this.visit_date.toLocaleString('en-US', {
+			dateStyle: 'medium',
+			timeZone: 'UTC',
+			timeStyle: 'short',
+			hour12: true,
+		})}.`;
 
-		// Create a transporter with your service provider, and your auth info
-		const transporter = nodemailer.createTransport({
-			service: 'gmail',
-			auth: {
-				user: process.env.EMAIL,
-				pass: process.env.EMAIL_PASSWORD,
-			},
-		});
+		this.send_mail(patient, doctor, subject, text);
+	});
+};
 
-		// Fill out mail options with the infromation needed
-		const mailOptions = {
-			from: 'kdujmic10@gmail.com',
-			to: `${patient[0].email}`,
-			subject: 'Doctor Appointment',
-			text: `${
-				patient[0].name
-			}, you have a scheduled appointment today with Dr.${
-				doctor[0].name
-			} at ${appointment.visit_date.toLocaleString('en-US', {
-				dateStyle: 'medium',
-				timeStyle: 'short',
-				hour12: true,
-			})}.`,
-		};
+exports.send_mail = (patient, doctor, subject, text) => {
+	const transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: process.env.EMAIL,
+			pass: process.env.EMAIL_PASSWORD,
+		},
+	});
 
-		// Send email or catch the error e.g. no valid email to send
-		transporter.sendMail(mailOptions, function (error, info) {
-			if (error) {
-				console.log(error);
-			} else {
-				console.log('Email sent: ' + info.response);
-			}
-		});
+	// Fill out mail options with the infromation needed
+	const mailOptions = {
+		from: 'kdujmic10@gmail.com',
+		to: `${patient[0].email}`,
+		subject: subject,
+		text: text,
+	};
+
+	// Send email or catch the error e.g. no valid email to send
+	transporter.sendMail(mailOptions, function (error, info) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log('Email sent: ' + info.response);
+		}
 	});
 };
