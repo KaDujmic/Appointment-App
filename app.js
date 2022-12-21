3530030426a002431595a9375235f7402b7d0273
Karlo Dujmic <kd48376@oss.unist.hr>
2022-12-21 11:50:45 +0100

    Update role in user schema from String to ObjectId

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookie_parser = require('cookie-parser');
 
 const user_routes = require('./routes/user_routes');
 const appointment_routes = require('./routes/appointment_routes');
+const role_routes = require('./routes/role_routes');
 
 const app = express();
 
@@ -14,5 +15,6 @@ app.use(cookie_parser());
 
 app.use('/users', user_routes);
 app.use('/appointments', appointment_routes);
+app.use('/roles', role_routes);
 
 module.exports = app;
