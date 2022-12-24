b0f242391014e789dc20daf82b78f85adf408dd3
Karlo Dujmic <kd48376@oss.unist.hr>
2022-12-24 14:44:34 +0100

    Add Router for finding a single object

--- routes/role_routes.js
+++ routes/role_routes.js
@@ -4,10 +4,16 @@ const authentication_controller = require('./../controllers/authentication_contr
 
 const router = express.Router({ mergeParams: true });
 
+router.use(authentication_controller.protect);
+
 router
 	.route('/')
 	.get(role_controller.get_all_roles)
 	.post(role_controller.create_role);
 
+router
+	.route('/:id')
+	.get(role_controller.get_role)
+	.put(role_controller.update_role);
 
-  module.exports = router;
+module.exports = router;
