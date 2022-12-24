91ced8ee11d7cc64a94105ebd6dabb94ac451a26
Karlo Dujmic <kd48376@oss.unist.hr>
2022-12-24 14:43:56 +0100

    Add Update Role function

--- controllers/role_controller.js
+++ controllers/role_controller.js
@@ -1,8 +1,7 @@
 const Role = require('../models/role_model');
 
-
 exports.get_all_roles = async (req, res) => {
-  try {
+	try {
 		const roles = await Role.find();
 		res.status(200).json({
 			status: 'success',
@@ -14,10 +13,10 @@ exports.get_all_roles = async (req, res) => {
 	} catch {
 		// err removed, breaks code when user not logged in. Protect function throws error and sends a response
 	}
-}
+};
 
 exports.get_role = async (req, res) => {
-  try {
+	try {
 		const role = await Role.findById(req.params.id);
 		res.status(200).json({
 			status: 'success',
@@ -31,25 +30,52 @@ exports.get_role = async (req, res) => {
 			msg: err.message,
 		});
 	}
-}
+};
 
 exports.create_role = async (req, res) => {
-  try {
-    if (req.user.role === 'admin') {
-      // const role_id = await Role.find({name: req.body.role})._id;
-      req.body.role = await Role.find({name: req.body.role})._id;
-      const role = await Role.create(req.body);
-      res.status(200).json({
-        status: 'success',
+	try {
+		if (req.user.role === 'admin') {
+			// const role_id = await Role.find({name: req.body.role})._id;
+			req.body.role = await Role.find({ name: req.body.role })._id;
+			const role = await Role.create(req.body);
+			res.status(200).json({
+				status: 'success',
 				data: {
 					role,
 				},
-      })
-    }
-  } catch (err) {
-    res.status(404).json({
+			});
+		}
+	} catch (err) {
+		res.status(404).json({
 			status: 'fail',
 			msg: err.message,
 		});
-  }
-}
+	}
+};
+
+exports.update_role = async (req, res) => {
+	try {
+		if (req.user.role.name === 'admin') {
+			const role = await Role.findByIdAndUpdate(
+				req.params.id,
+				req.body,
+				{
+					new: true,
+					runValidators: true,
+				}
+			);
+			res.status(200).json({
+				status: 'success',
+				data: {
+					role,
+				},
+			});
+		} else
+			throw new Error('You do not have permission to update a user!');
+	} catch (err) {
+		res.status(404).json({
+			status: 'fail',
+			msg: err.message,
+		});
+	}
+};
